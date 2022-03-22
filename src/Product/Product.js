2ee38b26d480ea9c81d40378962b1305f7143ac4
Yeasir-Hossain <yeasir06@gmail.com>
2022-03-23 01:00:40 +0600

    products shown

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -6,6 +6,12 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
+           <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price: {price}</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Rating: {ratings}</small></p>
+           </div>
         </div>
     );
 };
